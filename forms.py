2c5f37ddc05b6afae7340e01adc7e2837d3668df
Vincent Hokie <vincenthokie@gmail.com>
2017-08-13 16:24:39 +0300

    Flailing class definition of the SignUp form missed in last commit added

--- forms.py
+++ forms.py
@@ -9,6 +9,7 @@ class LoginForm(Form):
     username = StringField('username', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)class SignUpForm(Form):
+class SignUpForm(Form):
     username = StringField('username', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()])
     password2 = PasswordField('password2', validators=[DataRequired()])
