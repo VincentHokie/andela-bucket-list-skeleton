e696503adf5a3586ec5b1b996564efc628697bca
Vincent Hokie <vincenthokie@gmail.com>
2017-08-13 16:26:33 +0300

    Syntax error that began class definition on an occupied line corrected by putting class definition on a new line

--- forms.py
+++ forms.py
@@ -8,7 +8,8 @@ from wtforms.validators import DataRequired
 class LoginForm(Form):
     username = StringField('username', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()])
-    remember_me = BooleanField('remember_me', default=False)class SignUpForm(Form):
+    remember_me = BooleanField('remember_me', default=False)
+
 class SignUpForm(Form):
     username = StringField('username', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()])
