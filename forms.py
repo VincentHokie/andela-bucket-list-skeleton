013308c6e8740217826f173137d48e95fea6fc8e
Vincent Hokie <vincenthokie@gmail.com>
2017-08-13 16:22:33 +0300

    SignUp form logic created

--- forms.py
+++ forms.py
@@ -4,7 +4,12 @@ from flask_wtf import Form
 from wtforms import PasswordField, StringField, BooleanField, TextAreaField, DateField
 from wtforms.validators import DataRequired
 
+
 class LoginForm(Form):
     username = StringField('username', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()])
-    remember_me = BooleanField('remember_me', default=False)
+    remember_me = BooleanField('remember_me', default=False)class SignUpForm(Form):
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    password2 = PasswordField('password2', validators=[DataRequired()])
+
