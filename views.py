3f37fde4a77477e65f855b85a2bb0c1833d21092
Vincent Hokie <vincenthokie@gmail.com>
2017-08-13 11:49:09 +0300

    Initial Commit

---
+++ views.py
@@ -0,0 +1,43 @@
+__author__ = 'MacUser'
+
+from . import app
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return "Hello Worldj"
+
+
+@app.route("/index")
+def index():
+    return "Hello Worldj"
+
+
+@app.route("/index")
+def index():
+    return "Hello Worldj"
+
+
+@app.route("/index")
+def index():
+    return "Hello Worldj"
+
+
+@app.route("/index")
+def index():
+    return "Hello Worldj"
+
+
+@app.route("/index")
+def index():
+    return "Hello Worldj"
+
+
+@app.route("/index")
+def index():
+    return "Hello Worldj"
+
+
+@app.route("/index")
+def index():
+    return "Hello Worldj"
