f8f91b0a316580806a690b3df8665474dadc70c3
Vincent Hokie <vincenthokie@gmail.com>
2017-08-13 12:11:16 +0300

    Application routes created. Non-functional

--- views.py
+++ views.py
@@ -13,31 +13,62 @@ def index():
     return "Hello Worldj"
 
 
-@app.route("/index")
+@app.route("/sign-up", methods=['GET', 'POST'])
 def index():
     return "Hello Worldj"
 
 
-@app.route("/index")
+@app.route("/login", methods=['GET', 'POST'])
 def index():
     return "Hello Worldj"
 
 
-@app.route("/index")
+@app.route("/logout", methods=['GET'])
 def index():
     return "Hello Worldj"
 
 
-@app.route("/index")
+
+#bucket list crud routes
+@app.route("/create/bucket-list", methods=['GET', 'POST'])
 def index():
     return "Hello Worldj"
 
 
-@app.route("/index")
+@app.route("/update/bucket-list/<bucketListId>", methods=['GET', 'POST'])
 def index():
     return "Hello Worldj"
 
 
-@app.route("/index")
+@app.route("/view/bucket-lists", methods=['GET', 'POST'])
+def index():
+    return "Hello Worldj"
+
+
+@app.route("/delete/bucket-list/<bucketListId>", methods=['POST'])
+def index():
+    return "Hello Worldj"
+
+
+
+
+
+#bucket list items crud routes
+@app.route("/create/<bucketList>/item", methods=['GET', 'POST'])
+def index():
+    return "Hello Worldj"
+
+
+@app.route("/update/<bucketList>/item/<itemId>", methods=['GET', 'POST'])
+def index():
+    return "Hello Worldj"
+
+
+@app.route("/view/<bucketList>/items", methods=['GET', 'POST'])
+def index():
+    return "Hello Worldj"
+
+
+@app.route("/delete/bucket-list-item/<itemId>", methods=['POST'])
 def index():
     return "Hello Worldj"
