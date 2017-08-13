8665c7d7051a4c0237f4f6d496c1f35582baa56b
Vincent Hokie <vincenthokie@gmail.com>
2017-08-13 15:35:33 +0300

    Login form logic created

--- forms.py
+++ forms.py
@@ -2,4 +2,9 @@ __author__ = 'MacUser'
 
 from flask_wtf import Form
 from wtforms import PasswordField, StringField, BooleanField
-from  wtforms.validators import DataRequired
+from wtforms.validators import DataRequired
+
+class LoginForm(Form):
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    remember_me = BooleanField('remember_me', default=False)
